56da4d8b64e44f8d8ab8ff8ab679b6d8df27c483
t3mr <113214336+t3mr@users.noreply.github.com>
2023-03-11 23:30:03 +0300

    Add files via upload

---
+++ python projeleri/calculator.py
@@ -0,0 +1,66 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("1. Toplama")
+print("2. Çıkarma")
+print("3. Çarpma")
+print("4. Bölme")
+
+choice = input("Hangi işlemi yapmak istersiniz? (1/2/3/4): ")
+
+num1 = int(input("Birinci sayıyı girin: "))
+num2 = int(input("İkinci sayıyı girin: "))
+
+if choice == '1':
+    print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+    print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+    print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+    print(num1,"/",num2,"=", divide(num1,num2))
+else:
+    print("Geçersiz işlem seçimi")
+
+    from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+    operation = request.form['operation']
+    result = 0
+
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        result = num1 / num2
+
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='style.css') }}">
